93a624c3c97b7774a493331785d9e74f7fd21fd8
wsndbd <wsndbd@163.com>
2016-12-17 18:41:28 +0800

    add spider for smzdm with the basic tools

---
+++ spiderSmzdmBasic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#--*coding: utf-8*--
+#
+## file:   spiderSmzdmBasic.py
+## author: paldinzhang(paldinzhang@tencent.com)
+## date:   2016-12-17 18:40:41
+
+'''
+  Description:
+'''
+
